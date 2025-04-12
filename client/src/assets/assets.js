6333ee241ff7b5f955ff037ec78ecb19ca35fa04
Krishna Vamshi <vamshiburgula1609@gmail.com>
2025-04-12 08:29:11 +0530

    Updated app logo person logo

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,17 +1,19 @@
-import arrow_icon from './arrow_icon.svg'
-import lock_icon from './lock_icon.svg'
-import logo from './logo.svg'
-import mail_icon from './mail_icon.svg'
-import person_icon from './person_icon.svg'
-import hand_wave from './hand_wave.png'
-import header_img from './header_img.png'
+import arrow_icon from "./arrow_icon.svg";
+import lock_icon from "./lock_icon.svg";
+import logo from "./logo.png";
+import mail_icon from "./mail_icon.svg";
+import person_icon from "./person_icon.svg";
+import hand_wave from "./hand_wave.png";
+import header_img from "./header_img.jpg";
+import logout_icon from "./logout_icon.svg";
 
 export const assets = {
-    arrow_icon,
-    lock_icon,
-    logo,
-    mail_icon,
-    person_icon,
-    hand_wave,
-    header_img
-}
+  arrow_icon,
+  lock_icon,
+  logo,
+  mail_icon,
+  person_icon,
+  hand_wave,
+  header_img,
+  logout_icon,
+};
